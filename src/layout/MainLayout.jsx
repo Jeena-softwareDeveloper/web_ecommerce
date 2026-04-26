792ebe71701b48449b9274f61cdd9f0676e7fcce
Jeena-softwareDeveloper <jeena2284@gmail.com>
2026-04-26 12:52:36 +0530

    UI Update: Changed global background color to gray-100

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
+    <div className="min-h-screen bg-gray-100 flex flex-col">
       <main className="flex-grow py-2 md:py-4">
         <Outlet />
       </main>
